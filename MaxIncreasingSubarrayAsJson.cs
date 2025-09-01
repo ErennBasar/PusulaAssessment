5eff8292c8e9f2aa78fc4ae5c92e03448bb2f007
ErennBasar <2200656020@nku.edu.tr>
2025-09-01 21:59:30 +0300

    Add project files.

---
+++ MaxIncreasingSubarrayAsJson.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json;
+
+namespace PusulaAssessment
+{
+    class MaxIncreasingSubarrayAsJson
+    {
+        public static void Main()
+        {
+
+            var input4 = new List<int> { 1, 3, 5, 4, 7, 8, 2 };
+            //var input5 = new List<int>(); 
+
+            
+            Console.WriteLine(MaxIncreasingSubarray(input4));
+            //Console.WriteLine(MaxIncreasingSubarrayAsJson(input5)); 
+        }
+
+
+        public static string MaxIncreasingSubarray(List<int> numbers)
+        {
+            //if list is empty
+            if (numbers == null || numbers.Count == 0)
+            {
+                return "[]";
+            }
+
+            List<int> bestArray = new List<int>();
+            int bestSum = int.MinValue;
+
+            List<int> currentArray = new List<int>();
+            int currentSum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                // metoda gönderilen dizideki ilgili eleman ile currentArray'e eklenen son eleman karşılaştırılıyor
+                if (currentArray.Count == 0 || numbers[i] > currentArray[currentArray.Count - 1])
+                {
+                    currentArray.Add(numbers[i]);
+                    currentSum += numbers[i];
+                }
+                else
+                {
+                    if (currentSum > bestSum)
+                    {
+                        bestSum = currentSum;
+                        bestArray = new List<int>(currentArray);
+                    }
+
+                    // Artış bozulduğu için mevcut dizi sıfırlanıyor.
+                    // Yeni bir artan dizi bu elemandan itibaren başlatılıyor.
+                    currentArray.Clear();
+                    currentArray.Add(numbers[i]);
+                    currentSum = numbers[i];
+                }
+            }
+
+            //diziyi güncelleme
+            if (currentSum > bestSum)
+            {
+                bestSum = currentSum;
+                bestArray = new List<int>(currentArray);
+            }
+
+            return JsonSerializer.Serialize(bestArray);
+        }
+    }
+}
+
+    
+            
+
